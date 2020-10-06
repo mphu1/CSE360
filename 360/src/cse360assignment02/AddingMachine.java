bd6bfe2de07ecbb68209a315919ae220b4a92fd0
Michael <Michael@Michael-PC>
2020-10-05 21:41:52 -0700

    added what each method should do

--- 360/src/cse360assignment02/AddingMachine.java
+++ 360/src/cse360assignment02/AddingMachine.java
@@ -2,25 +2,46 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String history = "0"; // Variable for transaction history
+  private String op = ""; // Variable for the operation added to history
   
   public AddingMachine () {
-    total = 0;  // not needed - included for clarity
+    total = 0;  // Not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  public int getTotal () { // Returns total
+    return total;
   }
   
-  public void add (int value) {
+  public void add (int value) { // Add inputed value to total value
+	  total += value;
+	  op = " + " + value; 
+	  history += op;
   }
 
-  public void subtract (int value) {
+  public void subtract (int value) { // Subtract inputed value from total value
+	  total -= value;
+	  op = " - " + value;
+	  history += op;
   }
 
-  public String toString () {
-    return "";
+  public String toString () { // Return the history of transactions
+    return history;
   }
 
-  public void clear() {
+  public void clear() { // Clears the "memory"
+	  history = "0";
+	  total = 0;
+  }
+  public static void main(String Args[]) {
+	  AddingMachine add = new AddingMachine();
+	  
+	  add.add (4); 
+	  add.subtract (2); 
+	  add.add(5);
+	  
+	  System.out.println(add.toString());
   }
 }
+
+
